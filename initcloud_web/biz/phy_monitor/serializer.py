57f600ed9e755f5b1fbac3bd2c71ee03735023a5
CannedFish <lianggy0719@126.com>
2016-09-01 09:47:11 +0800

    phy_monitor serializer

--- initcloud_web/biz/phy_monitor/serializer.py
+++ initcloud_web/biz/phy_monitor/serializer.py
@@ -2,5 +2,17 @@
 
 from rest_framework import serializers
 
+class PhyNodeSerializer(serializers.Serializer):
+    id = serializers.CharField(max_length=64)
+    cpu1 = serializers.ListField(child=serializers.IntegerField())
+    cpu2 = serializers.ListField(child=serializers.IntegerField())
+    memory_voltage = serializers.ListField(child=serializers.IntegerField())
+
 class PhyMonitorSerializer(serializers.Serializer):
-    pass
+    nodes = PhyNodeSerializer()
+    disk = serializers.ListField(child=serializers.IntegerField())
+    disk_status = serializers.DictField(child=serializers.CharField())
+    electric_rota = serializers.ListField(child=serializers.IntegerField())
+    systemUI = serializers.ListField(serializers.FloatField())
+    PDU = serializers.ListField(child=serializers.ListField(\
+            child=serializers.FloatField()))
